209db0e27dd637b13b0ace0a5af7ecf12d0c4b7c
Kenjebai <142065688+Kenjebai@users.noreply.github.com>
2023-08-16 00:03:56 +0600

    Update home_works.js

--- javascript-project/js/home_works.js
+++ javascript-project/js/home_works.js
@@ -18,13 +18,74 @@ gmail_button.onclick = () => {
 // 2-дз
 
 const child_block = document.querySelector('.child_block');
-let px1 = 0;
-function right1(){
-    px1++;
-    child_block.style.left = `${px1}px`;
-    if(child_block.style.left == '446px'){
-        return false;
+
+let pxY = 0;
+let pxX = 0;
+let pxX2 = 446;
+let pxY2 = 446;
+let moveBlock
+
+moveBlock = () => {
+    if(pxX < 446){
+        pxX++
+        child_block.style.left = `${pxX}px`
+        setTimeout(moveBlock, 1) 
+    }else 
+    if(pxY < 446){
+        pxY++
+        child_block.style.top = `${pxY}px`
+        setTimeout(moveBlock, 1)
+    }else 
+    if(pxX2 > 0){
+        pxX2--
+        child_block.style.left = `${pxX2}px`
+        setTimeout(moveBlock, 1)
+    }else 
+    if(pxY2 > 0){
+        pxY2--
+        child_block.style.top = `${pxY2}px`
+        setTimeout(moveBlock, 1)
     }
-    right1();
 }
-right1();
+moveBlock()
+
+
+// 3-дз
+
+let interval = document.querySelector('.interval')
+let start = document.getElementById('start')
+let stop1 = document.getElementById('stop')
+let reset = document.getElementById('reset')
+let resume = document.getElementById('resume')
+
+let num = 0
+let count
+let c
+let s
+
+c = start.onclick = () => {
+    clearInterval(count)
+    count = setInterval(() => {
+    num++
+    interval.innerHTML = num
+}, 1000)
+}
+
+s = stop1.onclick = () => {
+    clearInterval(count)
+}
+
+resume.onclick = () => {
+    if(num == 0){
+        s()
+    }else{
+    c()
+    }
+}
+
+reset.onclick = () => {
+    location.reload()
+}
+
+
+
